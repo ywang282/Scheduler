b599c0ff4a1b542725da696e3f18ea3b80c74c05
Slater, Robert <rslater@illinois.edu>
2015-08-14 10:49:56 -0500

    Updated alert script so it php comment out code when not needed, instead of HTML comments.

--- alert.php
+++ alert.php
@@ -1,15 +1,18 @@
+<?php
+//To enable a manual alert, copy the commented out HTML and paste it and the bottom of this page, below the closing php end tag
+//and update ITEM TITLE GOES HERE and ITEM CONTENT GOES HERE with the relevant information
+/*
 <div id="alert-div">
-	<!--
 	<div class="alert alert-danger" role="alert">
 		<span class="alert-item-line show">
 		<span class="fa fa-exclamation-circle" aria-hidden="true"> </span>
 		<span class="sr-only">Warning:</span>
 		<span class="alert-item-title">
-		<strong>ITEM TITLE GOES HERE</strong>
+		<strong>ITEM TITLE GOES HERE: </strong>
 		</span>
-		,
 		<span class="alert-item-desc">ITEM CONTENT GOES HERE</span>
 		</span>
 	</div>
-	-->
 </div>
+*/
+?>
