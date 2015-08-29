c6ef3730535832bda5e186970569d4e0c230934b
William Weathers <weathrs2>
2015-08-29 11:17:07 -0500

    ran gulp build to create all versions of script and style

--- assets/unmin/scripts.js
+++ assets/unmin/scripts.js
@@ -3770,13 +3770,13 @@ $(".easy-search-text-input").keyup(function() {
 $( '#dropdown' ).change(function(){
   //console.log("new selection = ",$( '#dropdown' ).val());
   if ( $( '#dropdown' ).val() == "gen" ) {
-    console.log("dropdown value = ",$( '#dropdown' ).val());
+    //console.log("dropdown value = ",$( '#dropdown' ).val());
     $( '#search_everything' ).attr( 'action', 'http://search.grainger.uiuc.edu/discovery/splitsearch.asp');
     $( '#keyword' ).attr( 'name', 'searcharg' );
     $( '#search_everything .es-classic' ).prop( 'disabled', true);
     $( '#search_everything .es-bento' ).prop( 'disabled', false);
   } else {
-    console.log("dropdown value = ",$( '#dropdown' ).val());
+    //console.log("dropdown value = ",$( '#dropdown' ).val());
     $( '#search_everything' ).attr( 'action', 'http://search.grainger.illinois.edu/searchaid2/saresultsug.asp');
     $( '#keyword' ).attr( 'name', 'keyword' );
     $( '#search_everything .es-bento' ).prop( 'disabled', true);
