27cbc45953c2f7c3ed70f7a5774433da5b4d68b2
William Weathers <weathrs2>
2015-12-03 15:42:00 -0600

    shared_content submodule shared_footer.php updated.  resulting in a new shared_footer.php

--- shared_footer.php
+++ shared_footer.php
@@ -47,7 +47,7 @@
       <span class="footerheading">About this site</span>
       <span class="text-block"><a class="footer-link" href="http://www.vpaa.uillinois.edu/policies/web_privacy.cfm">Privacy</a></span>
       <div class="footer-block-top-space">
-        <a href="http://www.library.illinois.edu/doc/" aria-label="Federal Depository Library Program website link"><img src="./shared_content/assets/images/fdlp_logo.png" alt="Federal Depository Library Program Logo">
+        <a href="http://www.library.illinois.edu/doc/" aria-label="Federal Depository Library Program website link"><img src="./shared_content/assets/images/fdlp_logo.png" alt="Federal Depository Library Program Logo"></a>
       </div>
     </div> 
   </div>
