1713e9ea3469ce9e776ba47c1b11b69276213f65
William Weathers <weathrs2>
2015-12-09 16:10:18 -0600

    new dir preproc_content for preprocessed files to be processed gulp preprocess plugin.  preproc_head.php with query string variable to bust cache of style.css

---
+++ preproc_content/preproc_head.php
@@ -0,0 +1,23 @@
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <title>University Library, University of Illinois</title>
+    <link rel="stylesheet" href="./assets/built/style.css?v=/* @echo QUERY_STR */">
+    <link rel="stylesheet" href="http://cdn.jsdelivr.net/fancybox/2.1.5/jquery.fancybox.min.css">
+    <link rel="icon" href="./assets/favicon.png" type="image/png" />
+    <script src="//cdn.jsdelivr.net/g/jquery@2.1"></script>
+    <script>window.jQuery || document.write('<script src="./assets/js/jquery-1.10.2.min.js"><\/script>')</script>
+    <!--[if lt IE 9]>
+    <script src="./assets/js/respond.min.js"></script>
+    <script src="./assets/js/html5shiv.js"></script>
+    <script src="./assets/js/css_browser_selector.js"></script>
+    <![endif]-->
+    <!--[if gte IE 8]>
+    <link href="./assets/css/ie.css" rel="stylesheet" type="text/css" />
+    <![endif]-->
+</head>
+
