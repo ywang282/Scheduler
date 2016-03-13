0167bcecb70bb867fcd8374132fa09ce380b4cac
William Weathers <weathrs2>
2016-03-13 11:42:51 -0500

    new file shared-global-nav.php. mobile utilities removed from shared_header.php shared_navbar.php deleted. shared global nav included in siteheader.php utilities added to style as well as new padding for header.

---
+++ shared-global-nav.php
@@ -0,0 +1,69 @@
+<nav id="ui-lib-global-nav" class="global-nav-menu row">
+  <div id="ui-lib-mobile-utilities" class="ui-lib-mobile-show">
+    <div>
+      <a href="https://vufind.carli.illinois.edu/vf-uiu/MyResearch/Home"><span class="fa fa-user"> </span> My Account</a>
+    </div>
+    <div>
+      <a data-toggle="collapse" data-parent="#site-header-controls-small" href="#site_search_collapse_mobile"> <span class="fa fa-search"></span> Site Search</a>
+      <div id="site_search_collapse_mobile" class="collapse site-header-control-group">
+        <form id="sitesearch" class="search form-inline" method="get" action="undefined" role="search">
+          <div class="form-group">
+            <input name="hl" value="en" type="hidden" />
+            <input name="safe" value="off" type="hidden" />
+            <input name="filter" value="0" type="hidden" />
+            <input id="searchQuery_mobile" class="form-control input-sm" maxlength="1000" size="25" name="q" placeholder="Search Library Webpages" type="text"  aria-required="true" aria-label="Enter keywords to search library web pages.  Press enter to submit."/>
+            <span class="sr-only">enter keywoards to search library web pages. Press enter to submit</span>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div>
+      <a href="http://www.library.illinois.edu/iwonder"><span class="fa fa-comments"> </span> Ask A Librarian</a>
+    </div>
+  </div>
+  <div id="ui-lib-global-menu" class="menu-top-level-menu-container">
+<ul id="menu-global" class="nav navbar-nav"><li id="menu-item-81" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-81 dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" aria-expanded="false">About <span class="caret"></span></a>
+<ul role="menu" class=" dropdown-menu">
+  <li id="menu-item-99" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-99" role="presentation"><a href="http://www.library.illinois.edu/people/" role="menuitem" tabindex="-1">Employee Directory</a></li>
+  <li id="menu-item-190" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-190" role="presentation"><a href="http://www.library.illinois.edu/people/directory.html" role="menuitem" tabindex="-1">Departmental Directory</a></li>
+  <li id="menu-item-191" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-191" role="presentation"><a href="http://illinois.edu/calendar/list/4092?cal=20160118&amp;skinId=1977" role="menuitem" tabindex="-1">Events &amp; Workshops</a></li>
+  <li id="menu-item-98" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-98" role="presentation"><a href="http://www.library.illinois.edu/administration/human/positions/employment.html" role="menuitem" tabindex="-1">Library Employment</a></li>
+  <li id="menu-item-243" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-243" role="presentation"><a href="http://www.library.illinois.edu/geninfo/history.html" role="menuitem" tabindex="-1">About Our Collections</a></li>
+  <li id="menu-item-333" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-333" role="presentation"><a href="http://www.library.illinois.edu/administration/services/policies/" role="menuitem" tabindex="-1">Policies</a></li>
+  <li id="menu-item-242" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-242" role="presentation"><a href="http://www.library.illinois.edu/friends/make-a-gift/" role="menuitem" tabindex="-1">Make a Gift</a></li>
+  <li id="menu-item-172" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-172" role="presentation"><a title="More about the library" href="https://wordpress-dev.library.illinois.edu/about/" role="menuitem" tabindex="-1"><span class="glyphicon More about the library"></span>&nbsp;MORE</a></li>
+</ul>
+</li>
+<li id="menu-item-369" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-369"><a href="https://wordpress-dev.library.illinois.edu/library-hours/">Libraries &amp; Hours</a></li>
+<li id="menu-item-105" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-105 dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" aria-expanded="false">Using the Library <span class="caret"></span></a>
+<ul role="menu" class=" dropdown-menu">
+  <li id="menu-item-103" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-103" role="presentation"><a href="http://www.library.illinois.edu/circ/policies/Policies.html#account" role="menuitem" tabindex="-1">Setup &amp; Use My Account</a></li>
+  <li id="menu-item-106" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-106" role="presentation"><a href="http://www.library.illinois.edu/#roomresli" role="menuitem" tabindex="-1">Study Room Reservations</a></li>
+  <li id="menu-item-107" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-107" role="presentation"><a href="http://www.library.illinois.edu/services/librarycomputing.html" role="menuitem" tabindex="-1">Print, Copy, Scan</a></li>
+  <li id="menu-item-230" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-230" role="presentation"><a href="http://www.library.illinois.edu/it/helpdesk/howto/vpn.html" role="menuitem" tabindex="-1">Connect From Off-Campus</a></li>
+  <li id="menu-item-199" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-199" role="presentation"><a title="More about using the library" href="https://wordpress-dev.library.illinois.edu/using/" role="menuitem" tabindex="-1"><span class="glyphicon More about using the library"></span>&nbsp;MORE</a></li>
+</ul>
+</li>
+<li id="menu-item-108" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-108 dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" aria-expanded="false">Help &amp; Services <span class="caret"></span></a>
+<ul role="menu" class=" dropdown-menu">
+  <li id="menu-item-112" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-112" role="presentation"><a href="http://www.library.illinois.edu/askus/" role="menuitem" tabindex="-1">Ask a Librarian</a></li>
+  <li id="menu-item-110" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-110" role="presentation"><a href="http://guides.library.illinois.edu/" role="menuitem" tabindex="-1">Guides</a></li>
+  <li id="menu-item-111" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-111" role="presentation"><a href="http://www.library.illinois.edu/ill" role="menuitem" tabindex="-1">Interlibrary Loan</a></li>
+  <li id="menu-item-113" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-113" role="presentation"><a href="http://illinois.edu/calendar/list/4068" role="menuitem" tabindex="-1">Savvy Researcher Workshops</a></li>
+  <li id="menu-item-200" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-200" role="presentation"><a title="More about library help and services" href="https://wordpress-dev.library.illinois.edu/help-services/" role="menuitem" tabindex="-1"><span class="glyphicon More about library help and services"></span>&nbsp;MORE</a></li>
+</ul>
+</li>
+<li id="menu-item-100" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-100 dropdown ui-lib-global-menu-top-highlight"><a href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" aria-expanded="false">Search Tools <span class="caret"></span></a>
+<ul role="menu" class=" dropdown-menu">
+  <li id="menu-item-101" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-101" role="presentation"><a href="http://search.grainger.illinois.edu/searchaid2/searchassist.asp" role="menuitem" tabindex="-1">Easy Search</a></li>
+  <li id="menu-item-235" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-235" role="presentation"><a href="http://vufind.carli.illinois.edu/vf-uiu/" role="menuitem" tabindex="-1">Library Catalog</a></li>
+  <li id="menu-item-234" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-234" role="presentation"><a href="https://i-share.carli.illinois.edu/uiu/cgi-bin/Pwebrecon.cgi?DB=local&amp;PAGE=rbSearch" role="menuitem" tabindex="-1">Course Reserves</a></li>
+  <li id="menu-item-256" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-256" role="presentation"><a href="http://sfx.carli.illinois.edu/sfxuiu/az" role="menuitem" tabindex="-1">Online Journals</a></li>
+  <li id="menu-item-102" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-102" role="presentation"><a href="http://search.grainger.uiuc.edu/linker/" role="menuitem" tabindex="-1">Article Locator</a></li>
+  <li id="menu-item-104" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-104" role="presentation"><a href="http://guides.library.illinois.edu/az.php" role="menuitem" tabindex="-1">Databases (A-Z List)</a></li>
+  <li id="menu-item-201" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-145 current_page_item menu-item-201 active" role="presentation"><a title="More about search tools" href="https://wordpress-dev.library.illinois.edu/search-tools/" role="menuitem" tabindex="-1"><span class="glyphicon More about search tools"></span>&nbsp;MORE</a></li>
+</ul>
+</li>
+</ul>
+  </div>
+</nav>
--- shared_header.php
+++ shared_header.php
@@ -15,11 +15,13 @@
           </div>
         </div>
       </div>
+      
       <div class="col-xs-2 visible-xs">
         <a id="mobile-canvas-button" class="pull-right" href="javascript:void(0)">
-          <i class="fa fa-bars"><span class="sr-only">Site Navigation Menu</span> </i>
+          <span class="sr-only">Site Navigation Menu</span>
         </a>
       </div>
+
       <div class="col-sm-6 col-xs-12 site-utilities">
         <form id="sitesearch" class="search form-inline pull-right hidden-xs" method="get" action="search.php" role="search">
           <div class="input-group">
@@ -36,24 +38,6 @@
           <input name="filter" value="0" type="hidden" />
           <a id="myaccount" class="btn btn-default btn-sm dark-button" href="https://vufind.carli.illinois.edu/vf-uiu/MyResearch/Home"><span class="fa fa-user" aria-labelledby="my account"></span> My Account</a>
         </form>
-        <div id="site-header-controls-small" class="text-center hidden-md hidden-lg hidden-sm">
-          <div class="btn-group btn-group-xs">
-            <a type="button" class="btn btn-default site-header-control-group dark-button" href="https://vufind.carli.illinois.edu/vf-uiu/MyResearch/Home">My Account</a>
-            <a class="btn btn-default site-header-control-group dark-button" role="widget" onclick="open('http://www.library.illinois.edu/iwonder/index.php', 'chat', 'width=400,height=440,resizable=yes');return false;" href="http://www.library.illinois.edu/iwonder" target="_blank">Ask A Librarian</a>
-            <a class="btn btn-default site-header-control-group dark-button" data-toggle="collapse" data-parent="#site-header-controls-small" href="#site_search_collapse_mobile">Site Search</a>
-          </div>
-          <div id="site_search_collapse_mobile" class="collapse site-header-control-group">
-            <form id="sitesearch" class="search form-inline" method="get" action="search.php" role="search">
-              <div class="form-group">
-                <input name="hl" value="en" type="hidden" />
-                <input name="safe" value="off" type="hidden" />
-                <input name="filter" value="0" type="hidden" />
-                <input id="searchQuery_mobile" class="form-control input-sm" maxlength="1000" size="25" name="q" placeholder="Search Library Webpages" type="text"  aria-required="true" aria-label="Enter keywords to search library web pages.  Press enter to submit."/>
-                <span class="sr-only">enter keywoards to search library web pages. Press enter to submit</span>
-              </div>
-            </form>
-          </div>
-        </div>
       </div>
     </div>
   
