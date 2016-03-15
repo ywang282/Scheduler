1dae2d5b4ab6c7b223fe65263da79976e1db30cf
William Weathers <weathrs2>
2016-03-15 09:25:31 -0500

    footer style and html updates in shared_content preproc_footer and _head updated with new query string variables shared_footer and style files updated as a result of changed in shared content.

--- shared_footer.php
+++ shared_footer.php
@@ -1,53 +1,74 @@
-  <div class="row footer-info">
-    <h2 class="sr-only">library Website footer information</h2>
-    <div class="col-md-3 col-sm-3 col-xs-12" role="region" aria-label="contact us">
-      <div class="dropdown" aria-labelledby="address">
-        <a href="#" class="dropdown-toggle footer-link" data-toggle="dropdown" aria-label="contact us"><span class="footerheading">Contact Us:<b class="caret"></b></span></a>
+  <div id="ui-lib-shared-footer" class="row footer-info">
+    <div class="col-md-3 col-sm-3 col-xs-12" role="region">
+      <div class="dropdown" aria-labelledby="University library address" role="navigation">
+        <a href="#" class="dropdown-toggle footer-link" data-toggle="dropdown"><span class="footerheading">Contact Us:<b class="caret"></b></span></a>
         <ul class="dropdown-menu">
           <li><a href="http://www.library.illinois.edu/people/phone.php" aria-labelledby="Employee Directory">Employee Directory</a></li>
           <li><a href="http://www.library.illinois.edu/secondary/contactUs.html" aria-labelledby="General contact information">General contact information</a></li>
-          <li><a aria-label="Submit your feedback and suggestions" href="https://illinois.edu/fb/sec/1454867" aria-labelledby="Feedback and suggestions">Feedback and suggestions</a></li>
         </ul>
       </div>
-      <span class="text-block">University Library</span>
-      <span class="text-block">University of Illinois at Urbana-Champaign</span>
-      <span class="text-block">1408 W. Gregory Dr. | Urbana, IL 61801</span>
-      <span class="text-block">217.333.2290</span>
-      <div class="footer-block-top-space">
-        <span class="text-block"><a class="footer-link" href="https://www.facebook.com/universitylibrary">
-        <span class="fa fa-facebook fa-lg footer-icon-social" aria-label="facebook"></span><small> Find us on Facebook</small></a></span>
-        <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/friends/make-a-gift/">
-        <span class="fa fa-gift fa-lg footer-icon-social" aria-label="make a gift"></span><small> Make a Gift</small></a></span>
+      <address><strong>Address</strong>: University Library<br>
+      University of Illinois at Urbana-Champaign<br>
+      1408 W. Gregory Dr. | Urbana, IL 61801<br>
+      <strong>Phone</strong>: 217.333.2290<br>
+      </address>
+      <div class="footer-sm">
+        <ul class="socialmedia">
+            <li>      
+            <a href="https://www.facebook.com/universitylibrary">
+            <span class="fa fa-facebook-official fa-2x" aria-label="Univeristy of Illinois library facebook"></span></a>
+            </li>
+            <li>   
+            <a href="https://twitter.com/IllinoisLibrary">
+            <span class="fa fa-instagram fa-2x" aria-label="Univeristy of Illinois library Twitter"></span></a>
+            </li>
+            <li>   
+            <a href="https://www.instagram.com/uillinoislibrary/">
+            <span class="fa fa-twitter fa-2x" aria-label="Univeristy of Illinois library instagram"></span></a>
+            </li>
+        </ul>
+      </div>  
+      <div class="footer-gift">
+          <a href="http://www.library.illinois.edu/friends/make-a-gift/">
+          <span class="fa fa-gift fa-2x" aria-label="make a gift"></span> Make a Gift</a>
       </div>
     </div>
-    <div class="col-md-3 col-sm-3 col-xs-12" role="region" aria-label="information for">  
-      <span class="footerheading">Information for:</span>
-      <div class="row">
-        <div class="col-md-6"> 
-          <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/ugl/">Undergraduates</a></span>
-          <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/learn/users/gradstudents.html">Graduate Students</a></span>
-          <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/learn/users/faculty.html">Faculty</a></span>
-          <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/learn/users/alumni.html">Alumni</a></span>
-          <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/learn/users/visitors.html">Visitors</a></span>
+    <div class="col-md-3 col-sm-3 col-xs-12" aria-label="information for">  
+      <span class="footerheading" id="groups">Information for:</span>
+      <div class="row" role="navigation" aria-labelledby="groups">
+        <div class="col-md-6" id="info"> 
+          <ul class="usergroups">
+           <li><a class="footer-link" href="http://www.library.illinois.edu/ugl/">Undergraduates</a></li><br>
+           <li><a class="footer-link" href="http://www.library.illinois.edu/learn/users/gradstudents.html">Graduate Students</a></li>
+           <li><a class="footer-link" href="http://www.library.illinois.edu/learn/users/faculty.html">Faculty</a></li>
+           <li><a class="footer-link" href="http://www.library.illinois.edu/learn/users/alumni.html">Alumni</a></li>
+           <li><a class="footer-link" href="http://guides.library.illinois.edu/visitors">Visitors</a></li>
+          </ul>
         </div>
-        <div class="col-md-6">
-          <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/learn/users/affiliates.html">Affiliated Organizations</a></span>
-          <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/learn/users/usersdisabilities.html">Users with Disabilities</a></span>
-          <span class="text-block"><a class="footer-link" href="http://guides.library.illinois.edu/distance_learners/">Distance Learners</a></span>
-          <span class="text-block"><a class="footer-link" href="http://www.library.illinois.edu/staff/">Library Staff</a></span>
+        <div class="col-md-6" id="info">
+          <ul class="usergroups">
+           <li><a class="footer-link" href="http://www.library.illinois.edu/learn/users/affiliates.html">Affiliated Organizations</a></li>
+           <li><a class="footer-link" href="http://guides.library.illinois.edu/usersdisabilities">Users with Disabilities</a></li>
+           <li><a class="footer-link" href="http://guides.library.illinois.edu/distance_learners/">Distance Learners</a></li>
+           <li><a class="footer-link" href="http://www.library.illinois.edu/staff/">Library Staff</a></li>
+          <ul> 
         </div>
       </div>
     </div>
-    <div class="col-md-3 col-sm-3 col-xs-12" role="region" aria-label="feedback">
+    <div class="col-md-3 col-sm-3 col-xs-12" id="feedback">
       <span class="footerheading">Feedback</span>
-      <span class="text-block"><a class="footer-link" href="https://illinois.edu/fb/sec/887006">Suggest an item for purchase</a></span>
-      <span class="text-block"><a aria-label="Submit your feedback and suggestions" class="footer-link" href="https://illinois.edu/fb/sec/1454867">Give us feedback/suggestions</a></span>
+      <ul class="feedback">
+       <li><a class="footer-link" href="https://illinois.edu/fb/sec/887006">Suggest an item for purchase</a></li>
+       <li><a class="footer-link" href="https://illinois.edu/fb/sec/1454867">Give us feedback/suggestions</a></li>
+      </ul>
     </div>
     <div class="col-md-3 col-sm-3 col-xs-12" id="vertical" role="region" aria-label="about the site">
       <span class="footerheading">About this site</span>
-      <span class="text-block"><a class="footer-link" href="http://www.vpaa.uillinois.edu/policies/web_privacy.cfm">Privacy</a></span>
+      <ul class="about">
+       <li><a class="footer-link" href="http://www.vpaa.uillinois.edu/policies/web_privacy.cfm">Privacy</a></li>
+      </ul> 
       <div class="footer-block-top-space">
-        <a href="http://www.library.illinois.edu/doc/" aria-label="Federal Depository Library Program website link"><img src="./shared_content/assets/images/fdlp_logo.png" alt="Federal Depository Library Program Logo"></a>
+        <a href="http://www.library.illinois.edu/doc/"><img src="./shared_content/assets/images/fdlp_logo.png" alt="Federal Depository Library Program Logo"></a>
       </div>
     </div> 
   </div>
