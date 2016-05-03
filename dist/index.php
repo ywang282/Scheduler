9f9891a61b3e0765507c9439d6deb4cf7bc429a8
William Weathers <weathrs2@illinois.edu>
2016-05-03 12:00:08 -0500

    added rules for hover and focus states for buttons in orange tab region

--- dist/index.php
+++ dist/index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>University Library, University of Illinois</title>
-  <link rel="stylesheet" href="./assets/css/style-25fce4a420.css"> 
+  <link rel="stylesheet" href="./assets/css/style-16e0c4e2ef.css"> 
   <link rel="stylesheet" href="http://cdn.jsdelivr.net/fancybox/2.1.5/jquery.fancybox.min.css">
   <link rel="icon" href="./assets/images/favicon.png" type="image/png" />
   <script src="//cdn.jsdelivr.net/g/jquery@2.1"></script>
@@ -971,7 +971,7 @@
     </div>
   </script>
   <script type="text/javascript" src="./assets/js/get_time.php"></script>
-  <script type="text/javascript" src="./assets/js/script-6ba51c28a8.js"></script>
+  <script type="text/javascript" src="./assets/js/script-ced0e88798.js"></script>
   <script type="text/javascript">
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
